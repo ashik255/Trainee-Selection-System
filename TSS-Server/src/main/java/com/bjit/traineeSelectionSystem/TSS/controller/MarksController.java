ed64399e8b80a2d648bc484efef4e49c5b13f2e6
ashik255 <ariful15-11556@diu.edu.bd>
2023-07-26 02:13:31 +0600

    updates a marks Implementation in whole database

--- TSS-Server/src/main/java/com/bjit/traineeSelectionSystem/TSS/controller/MarksController.java
+++ TSS-Server/src/main/java/com/bjit/traineeSelectionSystem/TSS/controller/MarksController.java
@@ -9,25 +9,65 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/applicant")
+@RequestMapping("/marks/upload")
 @RequiredArgsConstructor
 public class MarksController {
 
     private final MarksService marksService;
-    @PostMapping("/uploadMarks")
-    public ResponseEntity<ResponseModel<?>> uploadMarks(@RequestBody MarksEntity marksEntity){
-        return marksService.uploadMarks(marksEntity);
+
+    @GetMapping("/all")
+    public ResponseEntity<ResponseModel<?>> allAttended(){
+        return marksService.allAttended();
+    }
+
+    @PostMapping("/written/{applicantId}/{mark}")
+    public ResponseEntity<ResponseModel<?>> uploadWrittenMarks(@PathVariable Long applicantId , @PathVariable Double mark){
+        return marksService.uploadWrittenMarks(applicantId , mark);
+    }
+    //again view all------------
+    @PostMapping("/aptitude/{applicantId}/{mark}")
+    public ResponseEntity<ResponseModel<?>> uploadAptitudeMarks(@PathVariable Long applicantId , @PathVariable Double mark){
+        return marksService.uploadAptitudeMarks(applicantId , mark);
+    }
+    //again view all---------------
+    @PostMapping("/written/aptitude/allow/{applicantId}")
+    public ResponseEntity<ResponseModel<?>> writtenAptitudeAllowed(@PathVariable Long applicantId){
+        return marksService.writtenAptitudeAllowed(applicantId);
+    }
+
+    @GetMapping("/written/aptitude/passed")
+    public ResponseEntity<ResponseModel<?>> writtenAptitudePassed(){
+        return marksService.writtenAptitudePassed();
+    }
+
+    @PostMapping("/technical/{applicantId}/{mark}")
+    public ResponseEntity<ResponseModel<?>> uploadTechnicalMarks(@PathVariable Long applicantId , @PathVariable Double mark){
+        return marksService.uploadTechnicalMarks(applicantId , mark);
+    }
+    //view written_aptitude passed
+    @PostMapping("/technical/allow/{applicantId}")
+    public ResponseEntity<ResponseModel<?>> technicalAllowed(@PathVariable Long applicantId) {
+        return marksService.technicalAllowed(applicantId);
+    }
+
+    @GetMapping("/technical/passed")
+    public ResponseEntity<ResponseModel<?>> technicalPassed(){
+        return marksService.technicalPassed();
     }
 
 
-    @PutMapping("/update/circularId")
-    public ResponseEntity<ResponseModel<?>> updateMarks(@RequestParam Long marksId , @RequestBody MarksEntity marksEntity) {
-        return marksService.updateMarks(marksId , marksEntity);
+    @PostMapping("/hr/allow/{applicantId}")
+    public ResponseEntity<ResponseModel<?>> hrAllowed(@PathVariable Long applicantId){
+        return marksService.hrAllowed(applicantId);
     }
-
-    @GetMapping("/getAllCircular")
-    public ResponseEntity<ResponseModel<?>> getAllMarks() {
-        return marksService.getAllMarks();
+    //view technical passed
+    @PostMapping("/hr/{applicantId}/{mark}")
+    public ResponseEntity<ResponseModel<?>> uploadHrMarks(@PathVariable Long applicantId , @PathVariable Double mark){
+        return marksService.uploadHrMarks(applicantId , mark);
+    }
+    @GetMapping("/hr/passed")
+    public ResponseEntity<ResponseModel<?>> hrPassed(){
+        return marksService.hrPassed();
     }
 
 }
